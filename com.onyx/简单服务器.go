120515f7be6b93fe22733d51412695303d675d0c
onyx <changhe626@163.com>
2018-11-22 17:52:57 +0800

    2018年11月22日17:52:52

--- com.onyx/简单服务器.go
+++ com.onyx/简单服务器.go
@@ -30,3 +30,24 @@ func main() {
 	}
 
 }
+
+/**
+web工作方式的几个概念
+以下均是服务器端的几个概念
+Request：用户请求的信息，用来解析用户的请求信息，包括post、get、cookie、url等信息
+Response：服务器需要反馈给客户端的信息
+Conn：用户的每次请求链接
+Handler：处理请求和生成返回信息的处理逻辑
+
+
+http包执行流程
+1. 创建Listen Socket, 监听指定的端口, 等待客户端请求到来。
+2. Listen Socket接受客户端的请求, 得到Client Socket, 接下来通过Client Socket与客户端通信。
+3. 处理客户端的请求, 首先从Client Socket读取HTTP请求的协议头, 如果是POST方法, 还可能要读取客户端提
+交的数据, 然后交给相应的handler处理请求, handler处理完毕准备好客户端需要的数据, 通过Client
+Socket写给客户端。
+
+
+Go为了实现高并发和高性能, 使用了goroutines来处理Conn的读写事件, 这样每
+个请求都能保持独立，相互不会阻塞，可以高效的响应网络事件。这是Go高效的保证。
+ */
